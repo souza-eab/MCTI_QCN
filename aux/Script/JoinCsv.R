0aaf48b8c952b3f214b3892a89d49672b536cea5
Edriano Souza <79071899+souza-eab@users.noreply.github.com>
2023-10-20 12:52:48 -0300

    Update JoinCsv.R

--- aux/Script/JoinCsv.R
+++ aux/Script/JoinCsv.R
@@ -2,14 +2,11 @@
 library(dplyr)
 library(readr)
 
-
 # Lista de arquivos CSV na pasta
 csv_files <- list.files(pattern = ".csv")
 
 # Lista para armazenar os DataFrames
 data_frames <- list()
-
-# ...
 
 # Loop para ler e processar cada arquivo
 for (csv_file in csv_files) {
@@ -36,14 +33,20 @@ merged_data <- bind_rows(data_frames)
 df<- as.data.frame(merged_data)
 colnames(df)
 
-
 newNames <- c("system:index", "area_ha", "c_qcn_rectify", ".geo",        
               "Classe_MB_C8", "Bioma")
-
+names(df) <- newNames
 
 # Selecionar apenas algumas colunas
 df_write <- df[, c("Classe_MB_C8", "Bioma", "c_qcn_rectify", "area_ha")]
-names(df) <- newNames
+
 
 # Salvar o DataFrame combinado em um novo arquivo CSV
 write_csv(df_write, "df_area_qcn.csv")
+
+#Eg. summarise
+df_amz <- df %>% 
+  filter(Bioma == 'amz') %>% # Biome Name
+  group_by(Classe) %>%
+  summarise(mean_carbon = mean(carbono_qcn, na.rm = TRUE),
+            sum_areaHA = sum(area_km, na.rm = TRUE))
